e810997ee1406a3048bae4ae0c037c164b95decb
Virus <q.ospinamariajose@gmail.com>
2025-09-05 11:58:01 -0500

    Maria Jose Ospina Quiceno

---
+++ src/main/java/com/mycompany/parcials1/ParcialS1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parcials1;
+
+/**
+ *
+ * @author qospi
+ */
+public class ParcialS1 {
+
+}
